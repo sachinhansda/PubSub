8bae3fcb7e5ac8f22145e3081e97b30d27943a1b
sachinhansda-ally <sachin.hansda@ally.io>
2021-07-23 19:09:58 +0530

    initial structure

---
+++ pubsub.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func CreateTopic(topicID int) {
+
+}
+
+func DeleteTopic(TopicID int) {
+
+}
+
+func AddSubscription(topicID,SubscriptionID int) {
+
+}
+
+func DeleteSubscription(SubscriptionID int) {
+
+}
+
+func Publish(topicID int, message) {
+
+}
+
+func Subscribe(SubscriptionID int, SubscriberFunc) {
+
+}
+
+func UnSubscribe(SubscriptionID int) {
+
+}
+
+func Ack(SubscriptionID, MessageID int) {
+
+}
+
+func main() {
+	CreateTopic(topicID)
+  DeleteTopic(TopicID)
+  AddSubscription(topicID,SubscriptionID)
+  DeleteSubscription(SubscriptionID)
+  Publish(topicID, message)
+  Subscribe(SubscriptionID, SubscriberFunc)
+  UnSubscribe(SubscriptionID)
+  Ack(SubscriptionID, MessageID)
+}
